0f1c1fd4c34e36f9a61e1b8f31763c04444566e3
Divyanshu <divyanshu2002tyagi@gmail.com>
2024-08-12 12:58:12 +0530

    Added reuqired files

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY') or 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'mysql://root:55sS%401025@localhost/flashcards_db'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'mysql://if0_37089114:kyfcn05pT75jq@localhost/if0_37089114_flashcards_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
