338b63793ae14ca09c807a01449863c0ef2fb628
Divyanshu <divyanshu2002tyagi@gmail.com>
2024-08-12 13:29:11 +0530

    Updated

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://if0_37089114:kyfcn05pT75jq@localhost/if0_37089114_flashcards_db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://if0_37089114:kyfcn05pT75jq@sql302.infinityfree.com:3306/if0_37089114_flashcards_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # Add other configurations if needed
