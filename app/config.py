5f1b4a63f8779680bd362db2edc7d33671585e8f
divine_coder <72038689+github2python@users.noreply.github.com>
2024-08-12 14:42:57 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://if0_37089114--kyfcn05pT75jq@sql302.infinityfree.com:3306/if0_37089114_flashcards_db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://avnadmin:AVNS_LsNBq8OaIvzyyCRCboU@mysql-1727cf96-divyanshu2002tyagi-9e52.k.aivencloud.com:13964/defaultdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # Add other configurations if needed
