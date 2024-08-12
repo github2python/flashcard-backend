6518955eb54bd5bd4d79dda5d1643d8c845117c3
divine_coder <72038689+github2python@users.noreply.github.com>
2024-08-12 16:12:26 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://avnadmin:AVNS_LsNBq8OaIvzyyCRCboU@mysql-1727cf96-divyanshu2002tyagi-9e52.k.aivencloud.com:13964/defaultdb'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # Add other configurations if needed
